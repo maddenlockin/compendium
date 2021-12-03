f2159086c215226d342b31fa3b999b62253c125a
maddenlockin <maddenlockin@gmail.com>
2021-12-03 13:11:07 -0800

    card detail built out

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export default function Card() {
+export default function Card({card}) {
     return (
-        <div>
-            
-        </div>
-    )
+        <figure aria-label={`yu-gi-oh card ${card.card}`}>
+            <img src={card.url_image} alt={`$card.card`} />
+            <div className="card-details">
+                <h3 href={card.cardList}>{card.card}</h3>
+                <p>Archetype:{card.archetype}</p>
+                <p>Race:{card.race}</p>
+                <p>Type:{card.type}</p>
+                <p>Description:{card.desc}</p>
+            </div>
+        </figure>
+    );
 }
+
+Card.propTypes = {
+    card: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        card: PropTypes.string.isRequired,
+        cardList: PropTypes.string.isRequired,
+        archetype: PropTypes.string.isRequired,
+        race: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        url_image: PropTypes.string.isRequired,
+        desc: PropTypes.string.isRequired,
+    }).isRequired,
+};
