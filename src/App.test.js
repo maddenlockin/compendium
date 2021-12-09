1b7d59545d1bc39f536a9fa6a77142b54cee36b2
maddenlockin <maddenlockin@gmail.com>
2021-12-09 12:18:03 -0800

    added new App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders app', () => {
+    render(<App />);
+    const el = screen.getByText(/Card Compendium/i);
+    expect(el).toBeInTheDocument();
+})
